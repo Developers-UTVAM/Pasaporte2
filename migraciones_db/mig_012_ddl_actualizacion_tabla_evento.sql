b6d85d894f8994ad9a2405d6dc5a9601b43cbaaa
Nicolas Hernandez <niTO6776@GMAIL.com>
2026-03-07 11:57:37 -0600

    insert de migraciones

--- migraciones_db/mig_012_ddl_actualizacion_tabla_evento.sql
+++ migraciones_db/mig_012_ddl_actualizacion_tabla_evento.sql
@@ -1,2 +1,7 @@
 alter Table evento ADD responsable_interno VARCHAR(200) NULL;
 alter Table evento ADD responsable_externo VARCHAR(200) NULL;
+
+INSERT INTO `migraciones` (`tipo`, `nombre`, `descripcion`, `archivo`)
+VALUES ('DDL', 'Actualizar tabla evento', 'Agregar columnas de responsable interno y externo a la tabla evento', 'mig_012_ddl_actualizacion_tabla_evento.sql');
+
+-- FIN DE LA MIGRACION
--- migraciones_db/mig_013_dll_alterar_contrasena_usuario.sql
+++ migraciones_db/mig_013_dll_alterar_contrasena_usuario.sql
@@ -1,1 +1,4 @@
 ALTER TABLE `usuario` MODIFY COLUMN `password` VARCHAR(255) NOT NULL;
+
+INSERT INTO `migraciones` (`tipo`, `nombre`, `descripcion`, `archivo`)
+VALUES ('DDL', 'Modificar columna password', 'Modificar columna password de la tabla usuario', 'mig_013_dll_alterar_contrasena_usuario.sql');
