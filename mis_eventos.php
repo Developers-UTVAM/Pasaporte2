ce13eac521c478893fc90c7b2f2aea855a950c28
Jonathan <203110239@utvam.edu.mx>
2026-04-16 21:17:14 -0600

    Correccion de link de mis eventos

--- mis_eventos.php
+++ mis_eventos.php
@@ -1,6 +1,11 @@
 <?php
 include_once __DIR__ . "/init.php";
 
+if (!isset($_SESSION["current_user"])) {
+    header("Location: index.php");
+    exit();
+}
+
 startAPI(null, ["evento", "registroevento"]);
 
 $accion = getvar('accion');
